79545666f2c9325a7b4e873587cbfa8b5c5a953a
Mr-H-06 <he-jiajun@sjtu.edu.cn>
2025-05-07 19:38:32 +0800

    storage part

---
+++ cmake-build-debug/CMakeFiles/ticket_system.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ticket_system.dir/src/storage.cpp.o"
+  "CMakeFiles/ticket_system.dir/src/storage.cpp.o.d"
+  "ticket_system"
+  "ticket_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ticket_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
