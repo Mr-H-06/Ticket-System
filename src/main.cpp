07fb775e55564256d9cf1a69a7d365fd19a42f82
Mr-H-06 <he-jiajun@sjtu.edu.cn>
2025-06-13 01:45:20 +0800

    try:dealing TLE, modify the block_size

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,17 @@ int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
-  std::ifstream file("/mnt/c/Users/hejia/Desktop/Ticket System/1867/33.in");
-  freopen("out.out", "w", stdout);
+  //std::ifstream file("/mnt/c/Users/hejia/Desktop/Ticket System/1867/33.in");
+  //freopen("out.out", "w", stdout);
   std::string line;
   OrderManager order;
   UserManager user;
   TrainManager train;
-  user.clear();
-  order.clear();
-  train.clear();
-  while (std::getline(file, line)) {
-  //while (std::getline(std::cin, line)) {
+  //user.clear();
+  //order.clear();
+  //train.clear();
+  //while (std::getline(file, line)) {
+  while (std::getline(std::cin, line)) {
     char *t = strtok(const_cast<char *>(line.c_str()), " ");
     std::cout << t << ' ';
     char idx[12];
